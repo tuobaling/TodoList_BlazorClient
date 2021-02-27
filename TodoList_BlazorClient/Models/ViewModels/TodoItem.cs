3603dee67dfaf081e5de142eb7e2e9b5fbe082c4
wunhu <wunhu@DESKTOP-P6IQ2NC>
2021-02-27 13:16:56 +0800

    加入專案檔案。

---
+++ TodoList_BlazorClient/Models/ViewModels/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace TodoList_BlazorClient.Models.ViewModels
+{
+    [Table("TodoItem")]
+    public partial class TodoItem_ViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        public int ListId { get; set; }
+
+        [ForeignKey(nameof(ListId))]
+        [InverseProperty(nameof(TodoList_ViewModel.TodoItems))]
+        public virtual TodoList_ViewModel List { get; set; }
+    }
+}
---
+++ TodoList_BlazorClient/Models/ViewModels/TodoList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace TodoList_BlazorClient.Models.ViewModels
+{
+    [Table("TodoList")]
+    public partial class TodoList_ViewModel
+    {
+        public TodoList_ViewModel()
+        {
+            TodoItems = new HashSet<TodoItem_ViewModel>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [InverseProperty(nameof(TodoItem_ViewModel.List))]
+        public virtual ICollection<TodoItem_ViewModel> TodoItems { get; set; }
+    }
+}
